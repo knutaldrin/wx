4e76a36bbc0aadf25b5b08fbfa6a803f8a613906
Knut Aldrin <buz@samfundet.no>
2018-12-07 22:59:12 +0100

    Add time for METAR/TAF, link to MI

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import requests
 import ephem
-from datetime import date, datetime
+from datetime import date, datetime, time
 app = Flask(__name__)
 
 @app.route('/')
@@ -19,14 +19,6 @@ def wx():
     next_rising = obs.next_rising(ephem.Sun(), use_center=True)
     next_setting = obs.next_setting(ephem.Sun(), use_center=True)
 
-    ## METAR/TAF
-
-    r = requests.get('https://api.met.no/weatherapi/tafmetar/1.0/?icao=ENDU&content_type=text/plain&content=metar')
-    metar = r.text.strip().splitlines()[-1]
-
-    r = requests.get('https://api.met.no/weatherapi/tafmetar/1.0/?icao=ENDU&content_type=text/plain&content=taf')
-    taf = r.text.strip().splitlines()[-1]
-
     rising_utc = next_rising.datetime().strftime("%H:%M")
     rising_lt  = ephem.localtime(next_rising).strftime("%H:%M")
     setting_utc = next_setting.datetime().strftime("%H:%M")
@@ -35,5 +27,33 @@ def wx():
     rising_time = "%sUTC (%sLT)" % (rising_utc, rising_lt)
     setting_time = "%sUTC (%sLT)" % (setting_utc, setting_lt)
 
-    return render_template('wx.html', rising=rising_time, setting=setting_time, metar=metar, metar_time="meh", taf=taf)
+    ## METAR/TAF
+
+    r = requests.get('https://api.met.no/weatherapi/tafmetar/1.0/?icao=ENDU&content_type=text/plain&content=metar')
+    metar = r.text.strip().splitlines()[-1]
+
+    r = requests.get('https://api.met.no/weatherapi/tafmetar/1.0/?icao=ENDU&content_type=text/plain&content=taf')
+    taf = r.text.strip().splitlines()[-1]
+
+    # Shittiest code ever, but works
+    metar_hrs, metar_min = (int(metar[7:9]), int(metar[9:11]))
+    taf_hrs, taf_min = (int(taf[7:9]), int(taf[9:11]))
+
+    metar_time = datetime.combine(date.today(), time(metar_hrs, metar_min))
+    taf_time = datetime.combine(date.today(), time(taf_hrs, taf_min))
+
+    metar_time_str = metar_time.strftime("%H:%M")
+    taf_time_str = taf_time.strftime("%H:%M")
+
+    # TODO: Clean up and factor out this ugly formatting code
+    metar_ago = (datetime.utcnow() - metar_time).total_seconds() // 60
+    metar_ago_str = "%d %s ago" % (metar_ago, "minute" if metar_ago == 1 else "minutes")
+
+    taf_ago = (datetime.utcnow() - taf_time).total_seconds() // 60
+    taf_ago_str = "%d %s ago" % (taf_ago, "minute" if taf_ago == 1 else "minutes")
+
+    return render_template('wx.html', 
+            rising=rising_time, setting=setting_time, 
+            metar=metar, metar_time=metar_time_str, metar_ago=metar_ago_str,
+            taf=taf, taf_time=taf_time_str, taf_ago=taf_ago_str)
 
